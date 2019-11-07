780db4456f6b493891c8e5299f9d60ba054779d4
warren mcdonald <warrenjmac@mail.com>
2019-11-06 22:21:03 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?("?")
+    else
+      self.end_with?(".")
+    end
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    complex_string = self.split(/[.?!]/).delete_if {|w| w.size < 2}.size
+    complex_string
   end
 end
